4c4108e4f37f7bfb021c60b9e3396bfcb2e755ea
unparalleled-js <yingthi@live.com>
2021-09-24 11:40:41 -0500

    refactor: set project baseline

--- setup.py
+++ setup.py
@@ -1,9 +1,6 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-from setuptools import (  # type: ignore
-    setup,
-    find_packages,
-)
+from setuptools import find_packages, setup  # type: ignore
 
 extras_require = {
     "test": [  # `test` GitHub Action jobs uses this
@@ -18,19 +15,14 @@ extras_require = {
         "flake8>=3.8.3,<4.0",  # Style linter
         "isort>=5.7.0,<6.0",  # Import sorting linter
     ],
-    "doc": [
-        "Sphinx>=3.4.3,<4",  # Documentation generator
-        "sphinx_rtd_theme>=0.1.9,<1",  # Readthedocs.org theme
-        "towncrier>=19.2.0, <20",  # Generate release notes
-    ],
     "release": [  # `release` GitHub Action job uses this
         "setuptools",  # Installation tool
-        "setuptools-scm",  # Installation tool
         "wheel",  # Packaging tool
         "twine",  # Package upload tool
     ],
     "dev": [
         "commitizen",  # Manage commits and publishing releases
+        "pre-commit",  # Ensure that linters are run prior to commiting
         "pytest-watch",  # `ptw` test watcher/runner
         "IPython",  # Console for interacting
         "ipdb",  # Debugger (Must use `export PYTHONBREAKPOINT=ipdb.set_trace`)
@@ -41,7 +33,6 @@ extras_require = {
 extras_require["dev"] = (
     extras_require["test"]
     + extras_require["lint"]
-    + extras_require["doc"]
     + extras_require["release"]
     + extras_require["dev"]
 )
@@ -51,29 +42,31 @@ with open("./README.md") as readme:
 
 
 setup(
-    name="<PYPI_NAME>",
+    name="ape-ledger",
     use_scm_version=True,
     setup_requires=["setuptools_scm"],
-    description="""<PYPI_NAME>: <SHORT_DESCRIPTION>""",
+    description="""ape-ledger: Plugin for Ledger Hardware Wallet""",
     long_description=long_description,
     long_description_content_type="text/markdown",
     author="ApeWorX Ltd.",
     author_email="admin@apeworx.io",
-    url="https://github.com/ApeWorX/<REPO_NAME>",
+    url="https://github.com/ApeWorX/ape-ledger",
     include_package_data=True,
-    install_requires=[],  # NOTE: Add 3rd party libraries here
+    install_requires=[
+        "importlib-metadata ; python_version<'3.8'",
+    ],  # NOTE: Add 3rd party libraries here
     python_requires=">=3.6,<4",
     extras_require=extras_require,
-    py_modules=["<MODULE_NAME>"],
+    py_modules=["ape_ledger"],
     license="Apache-2.0",
     zip_safe=False,
     keywords="ethereum",
     packages=find_packages(exclude=["tests", "tests.*"]),
-    package_data={"<MODULE_NAME>": ["py.typed"]},
+    package_data={"ape_ledger": ["py.typed"]},
     classifiers=[
         "Development Status :: 3 - Alpha",
         "Intended Audience :: Developers",
-        "License :: OSI Approved :: MIT License",
+        "License :: OSI Approved :: Apache Software License",
         "Natural Language :: English",
         "Operating System :: MacOS",
         "Operating System :: POSIX",
